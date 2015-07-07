e0aa1386bb7ae04a99f9959edb7c0e4befef33bd
West Wang <west@misfit.com>
2015-07-07 18:05:47 +0800

    add test class to test infer

---
+++ app/src/main/java/com/example/android_test/Util.java
@@ -0,0 +1,11 @@
+package com.example.android_test;
+
+/**
+ * Created by west on 7/7/15.
+ */
+public class Util {
+    public void Test() {
+        String s1 = null;
+        String s2 = s1.toString();
+    }
+}
